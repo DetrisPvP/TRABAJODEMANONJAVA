29ee168e2becde3fc146bd1ea41f77da159491af
Ricardo Estrella <ricardozacdee@gmail.com>
2025-12-04 20:46:28 -0400

    Primer commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const nombre = prompt("¿Cuál es tu nombre?");
+const nacimiento = prompt("¿En qué año naciste?");
+
+
+const añoActual = 2025;
+let edad = null;
+
+
+// Validación básica
+if (nacimiento && !isNaN(nacimiento)) {
+edad = añoActual - parseInt(nacimiento);
+}
+
+
+const mensaje = edad !== null
+? `Hola, ${nombre}. Tienes ${edad} años.`
+: "No se pudo calcular la edad. Verifica los datos.";
+
+
+document.getElementById("resultado").textContent = mensaje;
